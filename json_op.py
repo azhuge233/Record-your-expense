b66d2e41733276d4824ef36f976f258abb4182c3
233 azhuge <azhuge786698994@gmail.com>
2018-12-18 14:36:48 +0800

    Auto-detect os
    Now it can auto-detects the os to choose the right path to use.

--- json_op.py
+++ json_op.py
@@ -1,7 +1,20 @@
-import json, os
+import json, os, platform
 
 tolcost_path = ".\\data\\tol-cost.json"
 precost_path = ".\\data\\pre-cost.json"
+tolcost_path_nonWin = "./data/tol-cost.json"
+precost_path_nonWin = "./data/pre-cost.json"
+
+
+def decide_path():
+	if platform.system() != "Windows":
+		tol_path = tolcost_path_nonWin
+		pre_path = precost_path_nonWin
+	else:
+		tol_path = tolcost_path
+		pre_path = precost_path
+		
+	return tol_path, pre_path
 
 
 def create_tolcost(path):
@@ -14,33 +27,35 @@ def create_tolcost(path):
 
 def create_precost(path):
 	tmpdic = dict({})
-	# tmpdic["test1"] = ["test1", "USD", "7.00", "Test1 Comment"]
-	# tmpdic["test2"] = ["test2", "USD", "1.00", "Test2 Comment"]
 	with open(path, "w", encoding='utf-8') as f:
 		json.dump(tmpdic, f)
 
 
 def open_file():
-	if not os.path.exists(tolcost_path):
-		create_tolcost(tolcost_path)
+	tol_path, pre_path = decide_path()
 	
-	if not os.path.exists(precost_path):
-		create_precost(precost_path)
+	if not os.path.exists(tol_path):
+			create_tolcost(tol_path)
 	
-	with open(tolcost_path, "r") as f:
+	if not os.path.exists(pre_path):
+			create_precost(pre_path)
+	
+	with open(tol_path, "r") as f:
 		toldic = json.load(f)
 	
-	with open(precost_path, "r") as f:
+	with open(pre_path, "r") as f:
 		predic = json.load(f)
 	
 	return toldic, predic
 
 
 def save_file(tol_data, pre_data):
+	tol_path, pre_path = decide_path()
+	
 	tol_js = json.dumps(tol_data)
 	pre_js = json.dumps(pre_data)
-	with open(os.path.join(tolcost_path), "w") as f:
+	with open(os.path.join(tol_path), "w") as f:
 		f.write(tol_js)
 		
-	with open(os.path.join(precost_path), "w") as f:
+	with open(os.path.join(pre_path), "w") as f:
 		f.write(pre_js)
